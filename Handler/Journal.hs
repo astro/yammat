848f00cfdfcd7c75bc4ae72a15c80b4f3926075c
nek0 <nek0@chelnok.de>
2015-04-09 21:39:39 +0200

    now added cash checks

--- Handler/Journal.hs
+++ Handler/Journal.hs
@@ -7,8 +7,19 @@ import Handler.Common
 getJournalR :: Handler Html
 getJournalR = do
   master <- getYesod
-  entries <- runDB $ selectList [] [Asc TransactionId]
+  rawEntries <- runDB $ selectList [] [Desc TransactionId, LimitTo 30]
+  entries <- return $ L.reverse rawEntries
   total <- return $ L.sum $ I.map (transactionAmount . entityVal) entries
+  timeLimit <- return $ transactionTime $ entityVal $ L.last $ entries
+  cashiers <- runDB $ selectList [CashierCreated <=. timeLimit] [Asc CashierId]
+  list <- return $ merge entries cashiers
   cashBalance <- getCashierBalance
   defaultLayout $ do
     $(widgetFile "journal")
+
+merge :: [Entity Transaction] -> [Entity Cashier] -> [Either Transaction Cashier]
+merge [] [] = []
+merge (t:ts) [] = (Left $ entityVal t) : merge ts []
+merge (t:ts) (c:cs)
+  | transactionTime (entityVal t) < cashierCreated (entityVal c) = (Left $ entityVal t)  : merge ts (c:cs)
+  | transactionTime (entityVal t) > cashierCreated (entityVal c) = (Right $ entityVal c) : merge (t:ts) cs
